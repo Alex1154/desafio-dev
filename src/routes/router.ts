03df3e32ea7e89130639f97c1d4e1dbef0078b0a
Joao Alexandre <aleramosfilho@gmail.com>
2021-11-01 20:41:48 -0300

    upload config

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,33 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import multer from "multer"
 
-const multerConfig = multer()
 const router = Router()
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./src/temp/uploads")
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-")
+    cb(null, fileName)
+  },
+})
+const multerConfig = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == "*/txt") {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      return cb(new Error("Only .txt format allowed!"))
+    }
+  },
+})
+router.get("/", (req: Request, res: Response) => {
+  res.render("index")
+})
 
-router.get("/", (req, res) => {
-  res.send("Hello World!")
+router.post("/temp/uploads", multerConfig.single("file"), (req, res, next) => {
+  res.send("file uploaded")
 })
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import router from "./routes/router"
+import path from "path"
 
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.json())
 app.use(router)
+app.set("views", path.join(__dirname, "views"))
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
