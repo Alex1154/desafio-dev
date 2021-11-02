29574cd4f33aca24d3e1be0d52e83449f2f48e6d
Joao Alexandre <aleramosfilho@gmail.com>
2021-11-02 14:34:46 -0300

    config data to use

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from "express"
 import multer from "multer"
+import { txtFiles } from "../services/setData"
+import fs from "fs"
+import readline from "readline"
+import { Readable } from "stream"
 
 const router = Router()
 const fileFilter = (req, file, cb) => {
@@ -10,6 +14,7 @@ const fileFilter = (req, file, cb) => {
     return cb(new Error("Only .txt files are allowed!"))
   }
 }
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./src/temp/uploads")
@@ -27,8 +32,46 @@ router.get("/", (req: Request, res: Response) => {
   res.render("index")
 })
 
-router.post("/temp/uploads", multerConfig.single("file"), (req, res, next) => {
-  res.send("file uploaded")
+router.post(
+  "/temp/uploads",
+  multerConfig.single("file"),
+  (req: Request, res: Response, next) => {
+    res.send("file uploaded")
+    storage
+  }
+)
+const file = []
+
+router.get("/txt/:id", async (req: Request, res: Response) => {
+  const id = req.params.id
+  const dirName = txtFiles[id].toString()
+
+  const file = readline.createInterface({
+    input: fs.createReadStream(`./src/temp/uploads/${dirName}`),
+    output: process.stdout,
+    terminal: false,
+  })
+  const fileObj: Object[] = []
+  for await (let line of file) {
+    const lineSplit = line
+
+    fileObj.push({
+      Type: Number(lineSplit.slice(0, 1)),
+      Date: new Date(
+        `${lineSplit.slice(1, 5)}-${lineSplit.slice(5, 7)}-${lineSplit.slice(
+          7,
+          9
+        )}`
+      ).toISOString(),
+      Value: Number(lineSplit.slice(9, 19)),
+      CPF: Number(lineSplit.slice(19, 30)),
+      Card: lineSplit.slice(30, 42),
+      Time: lineSplit.slice(42, 48),
+      Owner: lineSplit.slice(48, 62),
+      Name: lineSplit.slice(62, 81),
+    })
+  }
+  return res.json(fileObj)
 })
 
-export default router
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express"
-import router from "./routes/router"
+import { router } from "./routes/router"
 import path from "path"
 
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.json())
+
 app.use(router)
 app.set("views", path.join(__dirname, "views"))
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const port = process.env.SERVER_PORT || 3333
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
